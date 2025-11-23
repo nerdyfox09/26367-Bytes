e4692c04f2623872bd9df134fc3b75797020416e
taliafox <talia.fox@thewalkerschool.org>
2025-11-22 21:41:44 -0500

    tuning autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedFarAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedFarAuto.java
@@ -16,7 +16,7 @@ public class RedFarAuto extends LinearOpMode {
         myRobot = new Bytes_Robot(hardwareMap);
 
         // Mirror across field centerline (y changes sign)
-        Pose2d beginPose = new Pose2d(65, 22, Math.toRadians(0));
+        Pose2d beginPose = new Pose2d(62, 14, Math.toRadians(0));
 
         MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
 
@@ -25,8 +25,9 @@ public class RedFarAuto extends LinearOpMode {
         Actions.runBlocking(
                 drive.actionBuilder(beginPose)
 
-                        .splineToLinearHeading(new Pose2d(60, 15, Math.toRadians(30)), Math.toRadians(0)) // pick up more balls
-                        // Spin up outtake
+                        .splineToLinearHeading(new Pose2d(53, 4, Math.toRadians(-30)), Math.toRadians(0)) // pick up more balls
+
+                        // Run outtake to shoot
                         .stopAndAdd(() -> {
                             myRobot.leftOuttakeMotor.setVelocity(1000);
                             myRobot.rightOuttakeMotor.setVelocity(1000);
@@ -48,20 +49,18 @@ public class RedFarAuto extends LinearOpMode {
                         // Stop intake
                         .stopAndAdd(() -> myRobot.intakeMotor.setPower(0.0))
 
-                        // Mirror the pickup path across Y-axis (negative Y becomes positive)
-                        .splineToLinearHeading(new Pose2d(37, 30, Math.toRadians(90)), Math.toRadians(0)) // pick up more balls
+                        .splineToLinearHeading(new Pose2d(31, 17, Math.toRadians(90)), Math.toRadians(180)) // pick up more balls
 
                         // Run intake at new location
                         .stopAndAdd(() -> myRobot.intakeMotor.setPower(1.0))
 
-                        .strafeTo(new Vector2d(36, 48))
+                        .strafeTo(new Vector2d(34,48))
 
                         .waitSeconds(1)
 
                         .stopAndAdd(() -> myRobot.intakeMotor.setPower(0.0))
 
-                        // Return mirrored path
-                        .splineToLinearHeading(new Pose2d(58, 13, Math.toRadians(-42)), Math.toRadians(-20))
+                        .splineToLinearHeading(new Pose2d(53, 4, Math.toRadians(-30)), Math.toRadians(20)) // drive back to line
 
                         // Spin up outtake flywheels
                         .stopAndAdd(() -> {
@@ -74,7 +73,7 @@ public class RedFarAuto extends LinearOpMode {
                         // Start intake to shoot
                         .stopAndAdd(() -> myRobot.intakeMotor.setPower(1.0))
 
-                        .waitSeconds(3)
+                        .waitSeconds(2)
 
                         // Stop outtake
                         .stopAndAdd(() -> {
@@ -87,8 +86,7 @@ public class RedFarAuto extends LinearOpMode {
 
                         .lineToX(30)
 
-                        .build()
-        );
+                        .build());
 
         // Save pose for TeleOp
         BYTES_GLOBAL_Storage.currentPose = drive.localizer.getPose();
